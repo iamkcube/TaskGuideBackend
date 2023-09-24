63f68047b397348daa98e3b062187f17def264cb
Kalinga <kalingakhatua@gmail.com>
2023-09-24 14:40:57 +0530

    fixed package errors

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ from langchain.chains import ConversationalRetrievalChain
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:5173",
+     "https://taskguide-ciphersix.web.app"])
 
-os.environ["OPENAI_API_KEY"] = os.environ["OPEN_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = os.environ["OPEN_API_KEY"]
+os.environ["OPENAI_API_KEY"] = "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"
 
 
 def get_pdf_text(pdf_docs):
